d04a6c84f45d622477e8882ffdb784f91dd0903b
程肖斌 <1540667353@qq.com>
2019-01-22 19:52:41 +0800

    bb

--- BBFrame/BBFrame/ViewController.m
+++ BBFrame/BBFrame/ViewController.m
@@ -17,7 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    //首先想好，是按照横向比例适配还是纵向适配。然后UI图上的坐标是多少，就直接写多少
+    CGRect frame = CGRectMake(100, 100, 120, 50);
+    UIView *sub  = [[UIView alloc]initWithFrame:fitRect(frame, NO)];//如果按照纵向适配尺寸，那么就用fitY(frame, NO)
+    sub.backgroundColor = UIColor.redColor;
+    [self.view addSubview:sub];
 }
 
 
